69bc99959170827d2fbdeeecc33657b3c956e208
Raúl Marticorena <rmartico@ubu.es>
2018-05-21 13:23:47 +0200

    Added GPL licenses and headers in source files.

--- config.py
+++ config.py
@@ -1,3 +1,20 @@
+#     Copyright (C) 2017-2018 UBU-ICCRAM-ADMIRABLE-NOVAMAG-GA686056
+#     http://crono.ubu.es/novamag
+#
+# This Program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This Program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with UBU-ICCRAM-ADMIRABLE-NOVAMAG-GA686056  see the file COPYING.  If not, see
+# <http://www.gnu.org/licenses/>.
+
 import os
 
 # Configuration parameters in SQLAlchemy
